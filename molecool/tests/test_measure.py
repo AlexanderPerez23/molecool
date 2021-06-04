51ee7f087e9aebbd1293ff60f8db9ab8f1cb4b0a
AlexanderPerez23 <84990602+AlexanderPerez23@users.noreply.github.com>
2021-06-04 15:13:25 -0400

    added more pytest functionalities

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def test_calculate_distance():
     """Test that calculate_distance function calculates what we expect."""
-    
+
     r1 = np.array([0, 0, 0])
     r2 = np.array([0, 1, 0])
 
@@ -17,6 +17,7 @@ def test_calculate_distance():
     calculated_distance = molecool.calculate_distance(r1, r2)
 
     assert expected_distance == calculated_distance
+
 
 def test_calculate_angle():
     """Test the calculate_angle function calculates what we expect"""
